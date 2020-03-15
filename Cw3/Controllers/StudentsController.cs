21c41172da1ca3cced26d846aa5384b49225a89d
Zaborejszyn <zabor1274@gmail.com>
2020-03-15 23:17:46 +0100

    Initial commit

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers {
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase {
+        [HttpGet]
+        public string GetStudent() {
+            return "Mateusz s19101";
+        }
+    }
+}
